93e9573dfdcff5b993a7f3bea7004d801685b64b
Mousecop <joaskew13@gmail.com>
2017-03-16 14:56:16 -0400

    add preventAndDispatch function. Map state

--- js/components/scoreboard.js
+++ js/components/scoreboard.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchScoreboard } from '../actions/index';
+import { fetchScoreboard, postUserScore } from '../actions/index';
+
+function preventAndDispatch(e, props) {
+  e.preventDefault();
+  props.dispatch(postUserScore({user:userInput.value, count: props.count}));
+}
+let userInput;
 
 export function Scoreboard(props) {
   return (
     <div>
       <h3>Leaderboard</h3>
       <ul>{props.scoreboard.map(score => <li>{score.user}{score.count}</li>)}</ul>
-      <form onSubmit="#">
-        <input type="text" placeholder="What's ya name bub?"/>
+      <form onSubmit={(e) => preventAndDispatch(e, props)}>
+        <input type="text" placeholder="What's ya name bub?" ref={ref => userInput=ref}/>
         <input type="submit" value="Submit my score!"/>
       </form>
     </div>
@@ -16,7 +22,8 @@ export function Scoreboard(props) {
 }
 
 const mapStateToProps = (state, props) => ({
-  scoreboard: state.scoreboard
+  scoreboard: state.scoreboard,
+  count: state.userGuesses.length
 });
 
 export default connect(mapStateToProps)(Scoreboard);
